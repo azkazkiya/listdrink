07611759cf118551fa2452dc2e8f350c1fad38aa
azkall <120769795+azkazkiya@users.noreply.github.com>
2024-11-03 19:02:48 +0700

    Add files via upload

---
+++ datacoffetime.php
@@ -0,0 +1,45 @@
+
+[
+    {
+        "id": "1",
+        "title": "Caffe Latte",
+        "detail": "Rich, full-bodied espresso in steamed milk, lightly topped with foam. A caffè latte is simply a shot or two of bold, tasty espresso with fresh, sweet steamed milk over it.",
+        "thumb": "",
+        "poster": ""
+    },
+    
+    {
+        "id": "2",
+        "title": "Cappucino",
+        "detail": "Espresso with steamed milk, topped with a deep layer of foam.",
+        "thumb": "",
+        "poster": ""
+    },
+    {
+        "id": "3",
+        "title": "Caffe Mocha",
+        "detail": "Espresso with bittersweet mocha sauce and steamed milk, topped with sweetened whipped cream. Delightful.
+        ",
+        "thumb": "",
+        "poster": ""
+    },
+    {
+        "id": "4",
+        "title": "Caramel Macchiato",
+        "detail": "Espresso combined with vanilla-flavoured syrup, milk and caramel sauce over ice. A Starbucks classic, chilled for a classic summer’s day. To our signature espresso we add a creamy mix of vanilla syrup and cold milk poured over ice; it’s then topped with our proprietary buttery caramel sauce. Sweet!
+        ",
+        "thumb": "",
+        "poster": ""
+    },
+    {
+        "id": "5",
+        "title": "Vanilla Sweet Cream Cold Brew",
+        "detail": "Just before serving, our slow-steeped Cold Brew is topped with a delicate float of house-made vanilla sweet cream that cascades throughout the cup.
+        ",
+        "thumb": "",
+        "poster": ""
+    },
+
+
+
+    ]
